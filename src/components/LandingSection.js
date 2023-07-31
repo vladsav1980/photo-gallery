799102a0bf043bd5f64a550ea5b0435881a090ed
vladsav1980 <vladsav1980@gmail.com>
2023-07-31 14:56:36 +0300

    first commit

---
+++ src/components/LandingSection.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { VStack, Box, Heading, Text, Image } from "@chakra-ui/react";
+import FullScreenSection from "./FullScreenSection";
+import heroImage from "../images/photo3.jpg";
+
+const LandingSection = () => {
+  return (
+    <FullScreenSection isDarkBackground backgroundColor="#14532d" spacing={8}>
+      <VStack w="90%" py={16} alignItems="center">
+        {/* Hero Image */}
+        <Image src={heroImage} height="60vmin" alt="Hero Image" />
+
+        {/* Hero Title */}
+        <Heading as="h1" fontSize="6vw" textAlign="center" mt={8}>
+          Unleash Your Photographic Journey with Our Photo Gallery
+        </Heading>
+
+        {/* Hero Text */}
+        <Text fontSize="4vmin" textAlign="center" mt={8}>
+          Welcome to the Photo Gallery, where photographers can showcase their
+          masterpieces, connect with a global audience, and turn their passion
+          into a profitable venture. Empowering photographers to share,
+          monetize, and expand their creative horizons, one snapshot at a time.
+          Capture the world, and let it capture you.
+        </Text>
+      </VStack>
+    </FullScreenSection>
+  );
+};
+
+export default LandingSection;
